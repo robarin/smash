d8f6e70a15bafcbfe1ddd29973cbf82e3e57e5d3
Sanjar <sanjar.berdaliev@gmail.com>
2021-04-14 16:49:00 +0600

    Proxy routes from rails to react

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
 
   protect_from_forgery prepend: true
+
+  def frontend
+    render file: "public/index.html", layout: false
+  end
 end
